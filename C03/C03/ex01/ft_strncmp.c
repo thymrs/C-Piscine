2cd6ffbb044452b4916fe7d391dd1cd5aeb71b62
thymrs <163648949+thymrs@users.noreply.github.com>
2025-07-20 20:29:32 +0700

    Add files via upload

---
+++ C03/C03/ex01/ft_strncmp.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: perattan <perattan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 16:17:47 by perattan          #+#    #+#             */
+/*   Updated: 2025/07/17 16:17:47 by perattan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (n <= 0)
+		return (0);
+
+	while (i < n && s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+
+// int	main(void)
+// {
+// 	char s1[] = "Hello";
+// 	char s2[] = "Hellz";
+
+// 	printf("%d", ft_strncmp(s1, s2, 4));
+// }
